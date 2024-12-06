d4d818820bb60f26043369f9546e4c75d082f8d6
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-06 10:54:10 +0100

    Base blank iOS project

---
+++ DebugSwiftAppDemo/DebugSwiftAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebugSwiftAppDemoApp.swift
+//  DebugSwiftAppDemo
+//
+//  Created by Javier Calatrava on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DebugSwiftAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DebugSwiftAppDemoTests/DebugSwiftAppDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DebugSwiftAppDemoTests.swift
+//  DebugSwiftAppDemoTests
+//
+//  Created by Javier Calatrava on 6/12/24.
+//
+
+import Testing
+@testable import DebugSwiftAppDemo
+
+struct DebugSwiftAppDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
